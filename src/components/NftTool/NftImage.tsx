c71e47328a71088ecd97d0d4bd62738adbf1904b
Joe Fitter <hello@joefitter.com>
2023-11-01 14:12:50 +0100

    fix this

--- src/components/NftTool/NftImage.tsx
+++ src/components/NftTool/NftImage.tsx
@@ -6,7 +6,9 @@ export function NftImage({ nft }: { nft: DigitalAssetWithJson }) {
     <img
       width="100%"
       src={
-        nft?.json?.image ? `https://cdn.magiceden.io/rs:fill:400:400:0:0/plain/${nft.json.image}` : "/blank-light.png"
+        nft?.json?.image
+          ? `https://img-cdn.magiceden.dev/rs:fill:400:400:0:0/plain/${nft.json.image}`
+          : "/blank-light.png"
       }
     />
   )
